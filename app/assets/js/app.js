b5225caa7e6040138518429a15138ec11abbe417
Brett Smith <brett@codeq.com>
2016-10-10 14:14:20 -0500

    finish implementing video-bg

--- app/assets/js/app.js
+++ app/assets/js/app.js
@@ -12,7 +12,23 @@ var centrePoint = (function($) {
 			video.append('<source src="assets/video/' + video.attr('data-src') + '.' + format + '" type="video/' + format + '">');
 		}
 
-		setVideoSrc('mp4');
+		setBackground = function() {
+			var images 			= ['cpc-hero-1', 'cpc-hero-2', 'cpc-hero-3', 'cpc-hero-4'],
+					rando = Math.floor(Math.random() * 4),
+					singleImage = images[rando],
+					backgroundImage = 'url(/assets/img/' + singleImage + '.jpg)'
+
+			$('.hero').css('background-image', backgroundImage);
+		}
+
+		// Don't load video if touch device
+		if (!Modernizr.touch) {
+			setVideoSrc('mp4');
+			setVideoSrc('webm');
+		} else {
+			setBackground();
+		}
+
 
   } // end init
 
